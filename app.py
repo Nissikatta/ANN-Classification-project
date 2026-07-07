012bd49cee3da47982a6c3c014a5cc537b8895a9
Nissi Katta <nissikatta25@gmail.com>
2026-07-07 18:33:19 +0530

    updated churn prediction app and model files

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ with open('scaler.pkl', 'rb') as file:
 st.title("Customer Churn Prediction")
 
 # User inputs
-geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
+geography = st.selectbox('Geography', ['France', 'Germany', 'Spain'])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
@@ -32,39 +32,32 @@ num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
 
-# Create input dataframe
-input_data = pd.DataFrame({
-    'CreditScore': [credit_score],
-    'Gender': [label_encoder_gender.transform([gender])[0]],
-    'Age': [age],
-    'Tenure': [tenure],
-    'Balance': [balance],
-    'NumOfProducts': [num_of_products],
-    'HasCrCard': [has_cr_card],
-    'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
-})
+if st.button("Predict"):
 
-# Geography one-hot encoding
-geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
-geo_encoded_df = pd.DataFrame(
-    geo_encoded,
-    columns=onehot_encoder_geo.get_feature_names_out(['Geography'])
-)
+    input_df = pd.DataFrame({
+        'CreditScore': [credit_score],
+        'Geography': [geography],
+        'Gender': [gender],
+        'Age': [age],
+        'Tenure': [tenure],
+        'Balance': [balance],
+        'NumOfProducts': [num_of_products],
+        'HasCrCard': [has_cr_card],
+        'IsActiveMember': [is_active_member],
+        'EstimatedSalary': [estimated_salary]
+    })
 
-# Combine all input features
-input_data = pd.concat([geo_encoded_df, input_data], axis=1)
+    input_df['Gender'] = label_encoder_gender.transform(input_df['Gender'])
 
-# Scale input
-input_data_scaled = scaler.transform(input_data)
+    transformed_input = onehot_encoder_geo.transform(input_df)
+    input_scaled = scaler.transform(transformed_input)
 
-# Prediction
-prediction = model.predict(input_data_scaled)
-prediction_proba = prediction[0][0]
+    prediction = model.predict(input_scaled)
+    prediction_proba = prediction[0][0]
 
-st.write(f'Prediction Probability: {prediction_proba:.2f}')
+    st.write(f"Prediction Probability: {prediction_proba:.2f}")
 
-if prediction_proba > 0.5:
-    st.write("The customer is likely to churn.")
-else:
-    st.write("The customer is not likely to churn.")
+    if prediction_proba > 0.5:
+        st.write("The customer is likely to churn.")
+    else:
+        st.write("The customer is not likely to churn.")
